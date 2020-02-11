5b41705a8af64d3a67328a03677eab702cd904b3
zenbuduh <zenbuduh@gmail.com>
2020-02-11 19:35:14 +0100

    first commit

---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+    <div class="container">
+    <h1>Welcome To Our Site</h1>
+    <p class="lead">Welcome to our brand new platform. This site uses Laravel v. 5.4</p>
+    </div>
+</div>
